5213206d1ca87a4ee77de6310df1f6ba4e38e65e
ALvin-progg <doctoralvim726@gmail.com>
2024-11-22 20:28:50 +0800

    Download button fixed

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
 // Hamburger Menu Func
 document.addEventListener("DOMContentLoaded", () => {
-    const navBtn = document.getElementById('navToggle')
-    const navCont = document.getElementById('navContent')
-    const navBtnRm = document.getElementById('navToggleRm')
+    const navBtn = document.getElementById('navToggle');
+    const navCont = document.getElementById('navContent');
+    const navBtnRm = document.getElementById('navToggleRm');
     navBtn.addEventListener('click', ()=> {
         navCont.classList.toggle("hidden");
     })
@@ -11,4 +11,19 @@ document.addEventListener("DOMContentLoaded", () => {
         navCont.classList.toggle("hidden");
     })
 }) 
+
+// download Resume/CV button Func
+document.addEventListener("DOMContentLoaded", ()=> {
+    const downloadBtn = document.getElementById('download').addEventListener (
+        'click' , ()=> {
+            const link = document.createElement('a')
+            link.href = '/assets/test.txt';
+            link.download = 'CV test.txt';
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        }
+    )
+}
+)
 
